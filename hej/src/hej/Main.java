c2e7fa2ede3c262ceeea910edaccaa9a85425941
ghghgh_ <hghghg600@gmail.com>
2016-02-05 11:39:06 +0100

    lägger till forreach

--- hej/src/hej/Main.java
+++ hej/src/hej/Main.java
@@ -3,8 +3,11 @@ package hej;
 public class Main {
 
 	public static void main(String[] args) {
-		//det här är en fin fil
+		// det här är en fin fil
 
+		for (String string : args) {
+
+		}
 	}
 
 }
